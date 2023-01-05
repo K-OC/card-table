c1d3e0bdcda5a7ce4cf9b1c117aa35de37afa09a
Kieran <kieranpoconnell@gmail.com>
2023-01-05 16:39:20 -0330

    [styling] table sizing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import Sandbox from "./Components/Sandbox";
-
+import styled from "styled-components";
 
 // Hello
 
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  padding: 0;
+  margin: 0;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <Wrapper className="App">
       <Sandbox></Sandbox>
-    </div>
+    </Wrapper>
   );
 }
 
